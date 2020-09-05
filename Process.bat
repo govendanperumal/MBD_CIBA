3cb8dc7cb9e8e50ecbc01ec7aeb1b08075612fdb
govendanperumal <66425521+govendanperumal@users.noreply.github.com>
2020-09-05 17:39:09 +0530

    Update Process.bat
    removed cd...

--- Process.bat
+++ Process.bat
@@ -7,15 +7,15 @@ REM echo "Model File Selected as: = %mname%
 REM echo "Stage number is: = %stage_num%
 
 IF %stage_num%==1 (
-	REM %matlabpath% -wait -log -nodesktop -nosplash -r "D:\1.Projects\ECU_Monitor_MIL_SIL; ip=getenv('mname');model_file=strtrim(ip);ret=UpdateMdl(model_file);pause(5);exit"
+	REM %matlabpath% -wait -log -nodesktop -nosplash -r "ip=getenv('mname');model_file=strtrim(ip);ret=UpdateMdl(model_file);pause(5);exit"
 	REM goto return_value
 )
 IF %stage_num%==2 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=UpdateMdl(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=UpdateMdl(model_file);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==3 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; ip=getenv('mname');model_file=strtrim(ip);ret=apply_config_setting(model_file,'New_config_setting');pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "ip=getenv('mname');model_file=strtrim(ip);ret=apply_config_setting(model_file,'New_config_setting');pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==4 (
@@ -23,27 +23,27 @@ IF %stage_num%==4 (
 	goto return_value
 )
 IF %stage_num%==5 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=SLDV_verifier(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=SLDV_verifier(model_file);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==6 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=apply_advisory_setting(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=apply_advisory_setting(model_file);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==7 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=TestCasesGenSLDV(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=TestCasesGenSLDV(model_file);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==8 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=MIL_Testing(model_file,1);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=MIL_Testing(model_file,1);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==9 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=SIL_Testing(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=SIL_Testing(model_file);pause(5);exit"
 	goto return_value
 )
 IF %stage_num%==10 (
-	%matlabpath% -wait -log -nodesktop -nosplash -r "cd D:\1.Projects\ECU_Monitor_MIL_SIL; BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=create_deviation_report(model_file);pause(5);exit"
+	%matlabpath% -wait -log -nodesktop -nosplash -r "BCM_C1A_EcuMonitor_Constant_2; ip=getenv('mname');model_file=strtrim(ip);ret=create_deviation_report(model_file);pause(5);exit"
 	goto return_value
 )
 
